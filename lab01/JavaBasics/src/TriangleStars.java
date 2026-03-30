4a0e6a208da0666905ea24b1b652f6d3e5dcb381
Dương Đức Duy <duongducduy825@gmail.com>
2026-03-30 13:28:21 +0700

    finish hw

---
+++ lab01/JavaBasics/src/TriangleStars.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TriangleStars {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int n;
+        do {
+            System.out.print("Enter n (height of triangle, positive integer): ");
+            while (!keyboard.hasNextInt()) {
+                System.out.print("Invalid input. Enter a positive integer: ");
+                keyboard.next();
+            }
+            n = keyboard.nextInt();
+        } while (n <= 0);
+
+        for (int i = 1; i <= n; i++) {
+            int spaces = n - i;
+            int stars = 2 * i - 1;
+
+            for (int s = 0; s < spaces; s++) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s < stars; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        keyboard.close();
+    }
+}
