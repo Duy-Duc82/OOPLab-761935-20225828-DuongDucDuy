434892ba1d9028ec9e2a8e8ae642f53ecf615937
Dương Đức Duy <duongducduy825@gmail.com>
2026-04-09 18:55:15 +0700

    Add Cart class and test in Aims

--- lab02/AimsProject/src/Aims.java
+++ lab02/AimsProject/src/Aims.java
@@ -1,4 +1,19 @@
 public class Aims {
     public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Animation", "Star Wars", 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Quentin Tarantino", "Drama", "Pulp Fiction", 14.99f);
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        System.out.println("Total cost: " + cart.totalCost());
+
+        cart.removeDigitalVideoDisc(dvd2);
+
+        System.out.println("Total cost after removal: " + cart.totalCost());
     }
 }
